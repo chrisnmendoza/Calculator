4433949beb811652bc93926533fd17ae393abc6d
Chris <mendozac@udel.edu>
2021-05-09 12:57:01 -0400

    Completed ALU

--- alu.vhdl
+++ alu.vhdl
@@ -5,23 +5,31 @@ use IEEE.numeric_std.all;
 entity alu is
 port(	A:	in std_logic_vector (7 downto 0);
         B:	in std_logic_vector (7 downto 0);
-		op: in std_logic_vector (1 downto 0); --00 = add, 01 = subtract, 10 = nothing, 11 = set on equal
+		opField: in std_logic_vector (1 downto 0); --This corresponds to the opfield in the 8-bit ISA instruction
 		O:	out std_logic_vector(7 downto 0);
         EQ: out std_logic
 );
 end alu;
 
 architecture behav of alu is
+signal temp: std_logic_vector(1 downto 0);
+signal sum: std_logic_vector(7 downto 0);
 begin
-process(A, B, op) is
+process(A, B, opField, temp, sum) is
 begin
-    EQ <= '0';
-	if(op = "00") then
-        O <= std_logic_vector(signed(A) + signed(B));
-    elsif(op = "01") then
-        O <= std_logic_vector(signed(A) + signed(not B) + 1);
-    elsif(op = "10") then
+    temp(1) <= opField(1) xor opField(0);
+    temp(0) <= (opField(1) and opField(0)) or (opField(1) and not opField(0));
 
+    EQ <= '0';
+	if(temp = "00") then
+        sum <= std_logic_vector(signed(A) + signed(B));
+        O <= sum;
+    elsif(temp = "01") then
+        sum <= std_logic_vector(signed(A) + signed(not B) + 1);
+        O <= sum;
+    elsif(temp = "10") then
+       --O <= oPrev;
+       --O <= A;
     else
         if(A = B) then
             EQ <= '1';
--- alu_tb.vhdl
+++ alu_tb.vhdl
@@ -10,7 +10,7 @@ architecture behav of alu_tb is
 component alu
 port(	A:	in std_logic_vector (7 downto 0);
         B:	in std_logic_vector (7 downto 0);
-		op: in std_logic_vector (1 downto 0); --00 = add, 01 = subtract, 10 = nothing, 11 = set on equal
+		opField: in std_logic_vector (1 downto 0); --This corresponds to the opfield of the 8-bit ISA instruction
 		O:	out std_logic_vector(7 downto 0);
         EQ: out std_logic
 );
@@ -21,7 +21,7 @@ signal op : std_logic_vector(1 downto 0);
 signal eq : std_logic;
 begin
 --  Component instantiation.
-alu1: alu port map (A => a, B => b, op => op, O => o, EQ => eq);
+alu1: alu port map (A => a, B => b, opField => op, O => o, EQ => eq);
 
 --  This process does the real job.
 process
@@ -34,8 +34,15 @@ end record;
 --  The patterns to apply.
 type pattern_array is array (natural range <>) of pattern_type;
 constant patterns : pattern_array := -- Order goes: Upper, Lower, Output, Overflow, Underflow, Select
-(("00000001", "00000001", "00000010", "00", '0'), --test 1 + 1 = 2
-("00000001", "00000001", "00000000", "01", '0')); --test 1 - 1 = 0
+(("10000001", "00000011", "10000100", "00", '0'), --test -127 + 3 = -124
+("00000001", "00000001", "00000010", "00", '0'), --test 1 + 1 = 2
+("00000011", "00000001", "00000010", "01", '0'), --test do nothing (notice how O output doesn't change)
+("00010000", "00000001", "00000010", "01", '0'), --test do nothing (notice how O output doesn't change)
+("11111111", "00000001", "00000010", "10", '0'), --test false equal (notice how O output doesn't change)
+("11110000", "11110000", "00000010", "10", '1'), --test true equal (notice how O output doesn't change)
+("00000011", "10000001", "10000100", "00", '0'), --test 3 + -127 = -124
+("00010000", "11111101", "00010011", "11", '0'), --test 16 - (-3) = 19
+("00000001", "00000001", "00000000", "11", '0')); --test 1 - 1 = 0
 begin
 --  Check each pattern.
 for n in patterns'range loop
