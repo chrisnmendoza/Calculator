7538511131331878f017917d1db2c4bf0e17b563
Chris <mendozac@udel.edu>
2021-05-23 23:02:38 -0400

    beq to work

--- calculator.vhdl
+++ calculator.vhdl
@@ -27,6 +27,13 @@ component alu is  --this is the alu need to add 1 because vhdl name
 );
 end component alu;
 
+component reg is
+    port(	I:	in std_logic_vector (7 downto 0) := "00000000";
+            clk:	in std_logic;
+            O:	out std_logic_vector(7 downto 0) := "00000000"
+    );
+end component reg;
+
 component registerFile is
     port(	rs1:	in std_logic_vector (1 downto 0); -- for loading addresses: 00 = register 1, 01 = register 2, 10 = register 3, 11 = register 4
 		rs2: in std_logic_vector (1 downto 0); -- for loading addresses
@@ -54,7 +61,7 @@ signal regFileOutputA, regFileOutputB: std_logic_vector(7 downto 0);
 signal aluInputA, aluInputB: std_logic_vector(7 downto 0);
 signal aluOpField: std_logic_vector(1 downto 0);
 signal aluOutput, aluOutputBuffer : std_logic_vector(7 downto 0);
-signal aluBranch: std_logic;
+signal aluBranchConfirm, aluBranchOutput: std_logic;
 signal rType : std_logic; --signifies whether it is add/subtract (1) or load (0)
 signal immExtend : std_logic_vector(7 downto 0); --immedate 0 or 1 padded
 signal opCodeAndClk: std_logic_vector(2 downto 0);
@@ -62,31 +69,62 @@ signal clk_sig1, clk_sig2, clk_sig3, clk_sig4, clk_sig5, clk_sig6, clk_sig7, clk
 signal printOrBranch, printing : std_logic := '0';
 signal rTypeAndPrinting : std_logic_vector(1 downto 0);
 
+--beq signals
+signal beqRegisterInputA, beqRegisterInputB, beqRegisterSelect : std_logic_vector (1 downto 0);
+signal beqRegisterOutputA, beqRegisterOutputB, beqRegisterWrite, beqRegisterDecremented : std_logic_vector(7 downto 0);
+signal branchCounter : std_logic_vector(1 downto 0) := "00";
+signal IBuffer : std_logic_vector(7 downto 0);
+
 --signal ws_signal : std_logic_vector (1 downto 0) := "00";
 begin  
-    regFile : registerFile port map(rs1 => regFileInputA, rs2 => regFileInputB, clk => clk_sig9, ws => regDest, wd=>regWrite, we => writeEnable,rd1 =>regFileOutputA, rd2 =>regFileOutputB  );
-    alu1: alu port map(A => aluInputA, B => aluInputB, opField => aluOpField, O => aluOutput, EQ => aluBranch);
 
-    with I(7 downto 6) select 
+    beqRegisterFile : registerFile port map(rs1 => "00", rs2 => "01", clk => clk_sig5, ws => "00", wd=>beqRegisterWrite, we => '1',rd1 =>beqRegisterOutputA, rd2 =>beqRegisterOutputB  );
+
+    --mux between 0 and eq value, select is I(1 downto 0)
+    with I(1 downto 0) select
+        aluBranchConfirm <= aluBranchOutput when "01",
+                            aluBranchOutput when "10",
+                            '0' when others;
+
+
+    --mux selected by aluBranchConfirm, between I(1 downto 0) and the decrementer to choose what to set the beqRegister to
+    with aluBranchconfirm select
+        beqRegisterWrite <= "000000" & I(1 downto 0) when '1',
+                            beqRegisterDecremented when others;
+
+    with beqRegisterOutputA select
+        beqRegisterDecremented <=   std_logic_vector(unsigned(beqRegisterOutputA) - 1) when "00000010", --two's complement subtraction
+                                    std_logic_vector(unsigned(beqRegisterOutputA) - 1) when "00000001", --two's complement subtraction
+                                    "00000000" when others;
+    
+    with beqRegisterOutputA select
+        IBuffer <=  I when "00000000",
+                    "00000000" when others;
+
+
+    regFile : registerFile port map(rs1 => regFileInputA, rs2 => regFileInputB, clk => clk_sig9, ws => regDest, wd=>regWrite, we => writeEnable,rd1 =>regFileOutputA, rd2 =>regFileOutputB  );
+    alu1: alu port map(A => aluInputA, B => aluInputB, opField => aluOpField, O => aluOutput, EQ => aluBranchOutput);
+
+    with IBuffer(7 downto 6) select 
         rType <= '0' when "01", --load is i type
                  '0' when "10", --print i guess
                  '1' when others;
     
     with rType select
-        regDest <= I(1 downto 0) when '1', --add/sub has regDest at last 2 bits
-                   I(5 downto 4) when others; --load is at 2 bits 2 to the left 
+        regDest <= IBuffer(1 downto 0) when '1', --add/sub has regDest at last 2 bits
+                   IBuffer(5 downto 4) when others; --load is at 2 bits 2 to the left 
     
-    regFileInputA <= I(5 downto 4);
+    regFileInputA <= IBuffer(5 downto 4);
 
     with rType select
-        regFileInputB <= I(3 downto 2) when '1', --add/sub format
-                         I(5 downto 4) when others; --just to have an expected return value, the registerFile will select rd as its A and B input on load
+        regFileInputB <= IBuffer(3 downto 2) when '1', --add/sub format
+                         IBuffer(5 downto 4) when others; --just to have an expected return value, the registerFile will select rd as its A and B input on load
 
-    with I(7 downto 6) select
+    with IBuffer(7 downto 6) select
         printOrBranch <= '1' when "10",
                          '0' when others;
     
-    with I(1 downto 0) select
+    with IBuffer(1 downto 0) select
         printing <= printOrBranch when "11",
                     '0' when others;
 
@@ -97,22 +135,22 @@ begin
                     immExtend when "00", --load gets regWrite from immediate field
                     regFileOutputA when others; --DO NOT CHANGE regWrite WHEN PRINTING = 1
     
-    with I(3) select
-        immExtend <= "0000" & I(3 downto 0) when '0',
-                     "1111" & I(3 downto 0) when others;
+    with IBuffer(3) select
+        immExtend <= "0000" & IBuffer(3 downto 0) when '0',
+                     "1111" & IBuffer(3 downto 0) when others;
 
-    aluOpField <= I(7 downto 6);
+    aluOpField <= IBuffer(7 downto 6);
 
     aluInputA <= regFileOutputA;
     aluInputB <= regFileOutputB;
 
-    with I(7 downto 6) select 
+    with IBuffer(7 downto 6) select 
         writeEnable <= '1' when "00",
                        '1' when "11",
                        '1' when "01",
                        '0' when others;
     
-    opCodeAndClk <= I(7 downto 6) & clk;
+    opCodeAndClk <= IBuffer(7 downto 6) & clk;
 
 
     process(clk)
@@ -155,13 +193,15 @@ begin
 
     process(clk_sig7)
     begin
-        clk_sig8 <= clk_sig7;
+        if(beqRegisterOutputA = "00000000") then
+            clk_sig8 <= clk_sig7;
+        end if;
     end process;
 
     process(clk_sig8)
     begin
         if(clk_sig8 = '1' and clk_sig8'event) then
-            --report "regWrite: " & integer'image(to_integer(signed(RegWrite))); --debugging for regWrite
+            report "regWrite: " & integer'image(to_integer(signed(RegWrite))); --debugging for regWrite
         end if;
         clk_sig9 <= clk_sig8;
     end process;
