aec5ed6c52d4c3281ed6467d81abb76ca0248e6c
Brandon Brooks <brooksbrandon81@gmail.com>
2021-05-26 18:39:55 -0400

    Added print statements to be formatted

--- printer.vhdl
+++ printer.vhdl
@@ -14,9 +14,27 @@ end printer;
 architecture behav of printer is
 begin
     process(clk)
+    variable int_val : integer;
     begin
-        if(clk'event and clk = '1' and en = '1') then --print 
-            report integer'image(to_integer(signed(I)));
+        if(clk'event and clk = '1' and en = '1') then
+        int_val := to_integer(signed(I));
+        if(int_val >= 0) then
+          if(int_val < 10) then
+            report "   " & integer'image(int_val) severity note;
+          elsif(int_val < 100) then
+            report "  " & integer'image(int_val) severity note;
+          else
+            report " " & integer'image(int_val) severity note;
+          end if;
+        else
+          if(int_val > -10) then
+            report "  " & integer'image(int_val) severity note;
+          elsif(int_val > -100) then
+            report " " & integer'image(int_val) severity note;
+          else
+            report integer'image(int_val) severity note;
+          end if;
+        end if;
         end if;
     end process;
 
